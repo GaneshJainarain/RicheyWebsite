4bec07f73143b80ea3ec3d2f54b1bb54025cf8f8
Ganesh Jainarain <RicheyJay2@gmail.com>
2021-03-13 20:12:01 -0500

    Added routes and pages in pages

--- richeys_photo/src/App.js
+++ richeys_photo/src/App.js
@@ -3,6 +3,9 @@ import React from 'react'
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Photos from './components/pages/Photos';
+
 
 function App() {
   return (
@@ -10,6 +13,8 @@ function App() {
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home}/>
+        <Route path='/About' exact component={About}/>
+        <Route path='/Photos' exact component={Photos}/>
       </Switch>
     
     </Router>
